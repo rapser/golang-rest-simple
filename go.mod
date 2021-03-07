4a8b1134f954fc2c26be4eb2d1972e143fc8bdf5
Miguel Angel Tomairo <matomairo@gmail.com>
2021-03-06 23:57:53 -0500

    configure update task

--- go.mod
+++ go.mod
@@ -3,13 +3,24 @@ module firstgo_app
 go 1.16
 
 require (
+	github.com/alexbrainman/sspi v0.0.0-20180613141037-e580b900e9f5 // indirect
 	github.com/go-pg/pg/v10 v10.7.7 // indirect
 	github.com/gofiber/fiber/v2 v2.5.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.2.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/klauspost/compress v1.11.12 // indirect
 	github.com/lib/pq v1.9.0 // indirect
 	github.com/rs/cors v1.7.0 // indirect
 	github.com/valyala/fasthttp v1.22.0 // indirect
 	golang.org/x/sys v0.0.0-20210305034016-7844c3c200c3 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/jcmturner/aescts.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/dnsutils.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
+	gopkg.in/jcmturner/gokrb5.v7 v7.5.0 // indirect
+	gopkg.in/jcmturner/rpc.v1 v1.1.0 // indirect
+	gorm.io/driver/postgres v1.0.8 // indirect
+	gorm.io/gorm v1.21.2 // indirect
 )
--- src/main.go
+++ src/main.go
@@ -17,19 +17,20 @@ func indexRoute(w http.ResponseWriter, r *http.Request) {
 
 func setupRoutes(app *fiber.App) {
 
-	app.Get("/api/v1/task", models.GetTasks)
-	app.Get("/api/v1/task/:id", models.GetTask)
-	app.Post("/api/v1/task", models.CreateTask)
-	app.Put("/api/v1/task/:id", models.UpdateTask)
-	app.Delete("/api/v1/task/:id", models.DeleteTask)
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	v1.Get("/task", models.GetTasks)
+	v1.Get("/task/:id", models.GetTask)
+	v1.Post("/task", models.CreateTask)
+	v1.Put("/task/:id", models.UpdateTask)
+	v1.Delete("/task/:id", models.DeleteTask)
 }
 
 func main() {
 
 	var port string = "3000"
-
 	app := fiber.New()
-
 	setupRoutes(app)
 
 	// router := mux.NewRouter()
@@ -41,14 +42,6 @@ func main() {
 	// apiRouter.HandleFunc("/todos/{id}", api.GetTodo).Methods("GET")
 	// apiRouter.HandleFunc("/todos/{id}", api.DeleteTodo).Methods("DELETE")
 	// apiRouter.HandleFunc("/todos/{id}", api.UpdateTodo).Methods("PUT")
-
-	// router := mux.NewRouter().StrictSlash(true)
-	// router.HandleFunc("/", indexRoute)
-	// apiRouter.HandleFunc("/tasks", api.).Methods("GET")
-	// apiRouter.HandleFunc("/tasks", Task.createTask).Methods("POST")
-	// router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
-	// router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
-	// router.HandleFunc("/tasks/{id}", updateTask).Methods("PUT")
 
 	fmt.Printf("Server running at port %s", port)
 	log.Fatal(app.Listen(":3000"))
